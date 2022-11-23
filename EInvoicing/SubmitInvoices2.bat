0b48da5b847920895a9c23da7fca4a8ae0160143
bendaryy <mohamedbendary1994@gmail.com>
2022-11-23 20:23:11 +0200

    dskljf

--- EInvoicing/SubmitInvoices2.bat
+++ EInvoicing/SubmitInvoices2.bat
@@ -1,2 +1,2 @@
-C:\laragon\www\sherif\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\sherif\EInvoicing 62657122 "Egypt Trust Sealing CA"
+C:\laragon\www\sherif\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\sherif\EInvoicing 62657222 "Egypt Trust Sealing CA"
 PAUSE
